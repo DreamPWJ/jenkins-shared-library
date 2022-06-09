66d4c1da62275d4869906d9c2e2d15e58ebf5f22
panweiji <406798106@qq.com>
2022-06-09 08:42:04 +0800

    chore: 增强Android流水线

--- src/shared/library/common/App.groovy
+++ src/shared/library/common/App.groovy
@@ -1,21 +1,23 @@
 package shared.library.common
 
+import shared.library.Utils
+
 /**
  * @author 潘维吉
  * @date 2022/1/9 13:22
  * @email 406798106@qq.com
  * @description App通用服务
- * 包含Android、iOS、Flutter、Unity、React Native等通用的方法
+ * 涵盖Android、iOS、Flutter、Unity、React Native等通用的方法
  */
 class App implements Serializable {
 
     /**
-     * 获取APP信息
+     * 获取APP信息需要的依赖安装
      */
-    static def getAppInfo(ctx) {
-        try {
-            ctx.sh "yargs"
-        } catch (error) {
+    static def getAppInfoPackageInit(ctx) {
+        // 查看当前项目的依赖模块  查看全局依赖模块命令是 npm ls -g --depth 0
+        def nodePackages = Utils.getShEchoResult(ctx, "npm ls --depth 0")
+        if (!nodePackages.contains("yargs")) {
             ctx.sh "npm i -D yargs"
             ctx.sh "npm install app-info-parser"
         }
