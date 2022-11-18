a67f0e34d242de88fe82664cb8f2d0acb0b80848
panweiji <406798106@qq.com>
2022-11-18 15:46:11 +0800

    chore: K8S集群支持NFS、挂载映射、多机不同配置文件等

--- src/shared/library/common/Kubernetes.groovy
+++ src/shared/library/common/Kubernetes.groovy
@@ -130,10 +130,12 @@ class Kubernetes implements Serializable {
                     volumeMountsItem.mountPath = nfsHostPath
                     volumeMountsItem?.other = "Name-2"
                 }
-                volumeMounts[1] = volumeMounts[0]
-                //volumeMounts.add(1,  volumeMounts[0])
-/*              volumeMounts[1]?.name = "nfsName-2"
-                volumeMounts[1]?.mountPath = "nfsHostPath-2" */
+
+                volumeMounts.add(1, volumeMounts[0])
+                volumeMounts[1].eachWithIndex { volumeMountsItem, index ->
+                    volumeMounts?.name = "nfsName-2"
+                    volumeMounts?.mountPath = "nfsHostPath-2"
+                }
 
                 volumes[0].eachWithIndex { volumesItem, index ->
                     volumesItem.name = nfsName
