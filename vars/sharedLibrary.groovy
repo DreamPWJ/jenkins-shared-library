8a8fc4a2e964e1c3eca522a09dc52e8586f822a5
panweiji <406798106@qq.com>
2022-11-21 16:10:03 +0800

    chore: K8S集群支持NFS、挂载映射、多机不同配置文件等

--- vars/sharedLibrary.groovy
+++ vars/sharedLibrary.groovy
@@ -539,7 +539,7 @@ def call(String type = 'web-java', Map map) {
                         docker {
                             // kubectl 环境  构建完成自动删除容器
                             image "dtzar/helm-kubectl:latest"
-                            args " -v /usr/bin/:/bin/ "
+                            args " -v /usr/bin/:/usr/bin/ "
                             reuseNode true // 使用根节点
                         }
                     }
