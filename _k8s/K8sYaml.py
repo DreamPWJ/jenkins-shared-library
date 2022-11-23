8333a9f70e9a245327d24e5ffef1e3e52151d9ea
panweiji <406798106@qq.com>
2022-11-23 10:01:07 +0800

    chore: K8S集群支持NFS、挂载映射、多机不同配置文件等

--- _k8s/K8sYaml.py
+++ _k8s/K8sYaml.py
@@ -1,4 +1,5 @@
 import sys
+import argparse
 from ruamel.yaml import YAML
 
 """
@@ -16,24 +17,89 @@ from ruamel.yaml import YAML
 # argArray = [int(arg1.split(","))]
 # print arg1
 
-yamlText = """\
-# example
-name:
-  # details
-  family: Smith   # very common
-  given: Alice    # one of the siblings
-"""
+# 传递参数  执行示例  python K8sYaml.py  --nfs_params=A,B,C --volume_mounts=AAA/name:BBB/path,CCC:BBB,DDD:BBB
+parser = argparse.ArgumentParser(description='manual to this script')
+parser.add_argument('--k8s_yaml_file', type=str, default="k8s.yaml")
+parser.add_argument('--nfs_params', type=str, default=None)
+parser.add_argument('--volume_mounts', type=str, default=None)
+args = parser.parse_args()
+k8s_yaml_file = args.k8s_yaml_file
 
-yaml = YAML()
-code = yaml.load(yamlText)
-code['name']['given'] = 'Bob'
-code['name']['me'] = 'panweiji'
-src_data = {'user': {'name': '潘维吉', 'age': 18, 'money': None, 'gender': True},
-            'lovers': ['柠檬小姐姐', '橘子小姐姐', '小可可']
-            }
-code['name']['item'] = []
-code['name']['item'].append(src_data)
-yaml.dump(code, sys.stdout)
+volume_mounts = args.volume_mounts
+volume_mounts_yaml = []
+volume_host_mounts_yaml = []
+if volume_mounts is not None:
+    print(volume_mounts)
+    volume_mounts_array = volume_mounts.split(",")
+    print(volume_mounts_array)
+
+    for index in range(len(volume_mounts_array)):
+        print(volume_mounts_array[index])
+        volume_mounts_name = "volume_mounts_" + str(index)
+        print(volume_mounts_name)
+        volume_mounts_path_array = volume_mounts_array[index].strip().split(":")
+        volume_mounts_host_path = volume_mounts_path_array[0]
+        volume_mounts_path = volume_mounts_path_array[1]
+        volume_mounts_yaml.append({"name": volume_mounts_name, "mountPath": volume_mounts_path})
+        volume_host_mounts_yaml.append({"name": volume_mounts_name, "hostPath": {"path": volume_mounts_host_path}})
+
+nfs_params = args.nfs_params
+nsf_mount_yaml = None
+nsf_server_yaml = None
+if nfs_params is not None:
+    print(nfs_params)
+    nfs_array = nfs_params.split(",")
+    print(nfs_array)
+    nfs_mount_path = nfs_array[0]
+    nfs_server = nfs_array[1]
+    nfs_server_path = nfs_array[2]
+    nfs_name = "nfs-storage"
+    nsf_mount_yaml = {"name": nfs_name, "mountPath": nfs_mount_path}
+    nsf_server_yaml = {"name": nfs_name, "nfs": {"server": nfs_server, "path": nfs_server_path}}
+
+# 第一步: 创建YAML对象
+yaml = YAML()  # typ='safe' 导致生成的yaml文件和原顺序不一致
+yaml.default_flow_style = False  # 按原风格输出
+
+# 第二步: 读取yaml格式的文件
+# with open('kubernetes.yaml', encoding='utf-8') as file:
+# data = yaml.load(file)  # 为列表类型
+yamlContent = list(yaml.load_all(open('kubernetes.yaml')))  # 多文档结构读取---分割的yaml文件
+
+yaml_volume_mounts = yamlContent[0]['spec']['template']['spec']['containers'][0]['volumeMounts'] = []
+yaml_volume = yamlContent[0]['spec']['template']['spec']['volumes'] = []
+
+yaml_volume_mounts.extend(
+    [nsf_mount_yaml, *volume_mounts_yaml]
+)
+
+yaml_volume.extend(
+    [nsf_server_yaml, *volume_host_mounts_yaml]
+)
+
+# print(yamlContent)
+
+with open('new.yaml', mode='w', encoding='utf-8') as file:
+    yaml.dump(yamlContent, file)
+
+# yamlText = """\
+# # example
+# name:
+#   # details
+#   family: Smith   # very common
+#   given: Alice    # one of the siblings
+# """
+#
+# yaml = YAML()
+# code = yaml.load(yamlText)
+# code['name']['given'] = 'Bob'
+# code['name']['me'] = 'panweiji'
+# src_data = {'user': {'name': '潘维吉', 'age': 18, 'money': None, 'gender': True},
+#             'lovers': ['柠檬小姐姐', '橘子小姐姐', '小可可']
+#             }
+# code['name']['item'] = []
+# code['name']['item'].append(src_data)
+# yaml.dump(code, sys.stdout)
 
 # # 第一步: 创建YAML对象
 # # yaml = YAML(typ='safe')
