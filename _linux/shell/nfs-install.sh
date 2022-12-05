1c159a1c640e8ab01e9ebf890224d5f7d325fded
panweiji <406798106@qq.com>
2022-12-05 13:36:08 +0800

    chore: K8S分布式存储之NFS网络文件系统

--- _linux/shell/nfs-install.sh
+++ _linux/shell/nfs-install.sh
@@ -2,7 +2,8 @@
 # Author: 潘维吉
 # Description: NFS分布式网络文件存储服务
 # 参考文章: https://cloud.tencent.com/developer/article/1914388
-# 对外分别开通NFS服务的tcp 111 2049 端口 udp 111 4046端口并确保IP白名单可访问NFS服务 并且默认客户端访问端口要小于1024
+# 对外分别开通NFS服务的tcp 111 2049 端口 udp 111 4046端口并确保IP白名单可访问NFS服务
+# 并且默认客户端访问端口要小于1024否则被NFS服务端拒绝 is waiting to start: ContainerCreating Pod直接无法启动了
 
 cd /my
 
@@ -16,7 +17,7 @@ sudo yum install -y rpcbind nfs-util || true
 
 # 创建要共享的目录
 mkdir -p /my/nfs/data/
-# 设置文件夹权限否则可能导致PVC创建失败  is waiting to start: ContainerCreating Pod直接无法启动了
+# 设置文件夹权限否则可能导致PVC创建失败
 chmod -R 777 /my/nfs/data/
 
 # 编辑NFS配置并加入以下内容  允许访问NFS服务器的网段，也可以写 * ，表示所有地址都可以访问NFS服务和权限
@@ -64,6 +65,5 @@ cat /var/lib/nfs/etab
 # 创建一个用于nfs共享目录的挂载点
 # sudo mkdir -p /mnt/nfs_shared_client
 # 挂在共享目录到客户端
-# sudo mount -t nfs NFSServerIP:/mnt/nfs_shared_server /mnt/nfs_shared_client
+# sudo mount -t nfs -o nolock NFSServerIP:/mnt/nfs_shared_server /mnt/nfs_shared_client
 # umount -f /mnt/nfs_shared_client
-# sudo mount -t nfs 58.58.179.242:/mnt/stor1 /mnt/stor1
