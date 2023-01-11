001395b586b6245cab1aeea119feabd4b3059a92
panweiji <406798106@qq.com>
2023-01-11 13:54:22 +0800

    chore: K8S分布式中间件集群部署

--- _linux/auto-ssh.sh
+++ _linux/auto-ssh.sh
@@ -5,7 +5,7 @@
 # 安全性高和定制化的数据建议保存为Jenkins的“Secret file”类型的凭据并获取 无需放在代码中
 
 # 建立免密连接 流水线已实现自动设置免密登录  如需手动设置步骤如下
-# 需要在jenkins docker容器内而非宿主机 ssh-keygen -t rsa   root用户在/root/.ssh/id_rsa.pub
+# 需要在Jenkins Docker ``容器内而非宿主机`` ssh-keygen -t rsa   root用户在/root/.ssh/id_rsa.pub
 # 公钥放在远程访问服务的/root/.ssh/authorized_keys里  在jenkins容器里执行 ssh root@ip 命令访问确认
 # 如果有跳板机情况 可手动将构建机器的公钥分别添加外网跳板机和内网目标机authorized_keys内实现免密登录 touch authorized_keys
 # 自动命令 scp -p ~/.ssh/id_rsa.pub root@<remote_ip>:/root/.ssh/authorized_keys && ssh root@<remote_ip> -p 22
@@ -34,3 +34,4 @@ done <hosts.txt
 
 # 透传跳板机实现自动登录授权
 # 主要思路是 1. 客户端执行机器先免密到跳板机 2. 跳板机再免密到目标机 3. 最后将客户端的公钥/root/.ssh/id_rsa.pub放到内网目标机/root/.ssh/authorized_keys授信
+# 在执行SSH跳板命令生效:  ssh -J root@外网跳板机IP:22 root@内网目标机器IP -p 22
--- src/shared/library/common/SecureShell.groovy
+++ src/shared/library/common/SecureShell.groovy
@@ -34,9 +34,9 @@ class SecureShell implements Serializable {
                 ctx.println "免密登录失败, 根据hosts.txt文件已有的账号信息自动设置, 如果没有配置hosts.txt请手动设置ssh免密登录"
                 ctx.println "如果有跳板机情况, 可手动将构建机器的公钥分别添加到外网跳板机和内网目标机authorized_keys内实现免密登录"
                 try {
-                    // 目的是清除当前机器里关于远程服务器的缓存和公钥信息 如远程服务器已重新初始化情况 导致本地还有缓存
+                    // 目的是清除当前机器里关于远程服务器的缓存和公钥信息 如远程服务器已重新初始化或升降配等情况 导致本地还有缓存
                     // ECDSA host key "ip" for  has changed and you have requested strict checking 报错
-                    ctx.sh "ssh-keygen -R ${ctx.remote.host}"
+                    ctx.sh "ssh-keygen -R ${ctx.remote.host}" // 未来实现跳板机的情况
                 } catch (e) {
                     ctx.println "清除当前机器里关于远程服务器的缓存和公钥信息失败"
                     ctx.println e.getMessage()
