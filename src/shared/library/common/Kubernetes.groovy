c25c6102764976564eea6e4e371c1431695fa7e0
panweiji <406798106@qq.com>
2023-02-16 15:07:14 +0800

    chore: 基于QPS部署Pod水平扩缩容

--- src/shared/library/common/Kubernetes.groovy
+++ src/shared/library/common/Kubernetes.groovy
@@ -169,7 +169,7 @@ class Kubernetes implements Serializable {
         ctx.sh "kubectl apply -f ${yamlName}"
         // 若安装正确，可用执行以下命令查询自定义指标 查看到 Custom Metrics API 返回配置的 QPS 相关指标  可能需要等待几分钟才能查询到
         ctx.sh " kubectl get --raw /apis/custom.metrics.k8s.io/v1beta1 || true "
-        // kubectl get --raw "/apis/custom.metrics.k8s.io/v1beta1/namespaces/default/pods/*/httpserver_requests_qps"
+        // kubectl get --raw "/apis/custom.metrics.k8s.io/v1beta1/namespaces/default/pods/*/http_server_requests_qps"
 
         // 并发测试ab（apache benchmark） CentOS环境 sudo yum -y install httpd-tools    Ubuntu环境 sudo apt-get update && sudo apt-get -y install apache2-utils
         // ab -c 100 -n 10000 -r http://120.92.49.178:8080/  // 并发数-c  总请求数-n  是否允许请求错误-r  总的请求数(n) = 次数 * 一次并发数(c)
