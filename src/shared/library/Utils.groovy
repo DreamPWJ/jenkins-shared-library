fa0a6b13004ba02e322a98380b657d874caf4b76
panweiji <406798106@qq.com>
2023-05-09 13:05:23 +0800

    chore: TIDB分布式数据库

--- src/shared/library/Utils.groovy
+++ src/shared/library/Utils.groovy
@@ -50,12 +50,11 @@ class Utils implements Serializable {
             versionNum = versionNum.replaceAll("v", "").replaceAll("V", "") // 去掉前缀
             def regex = '^(([0-9]|([1-9]([0-9]*))).){2}([0-9]|([1-9]([0-9]*)))([-](([0-9A-Za-z]|([1-9A-Za-z]([0-9A-Za-z]*)))[.]){0,}([0-9A-Za-z]|([1-9A-Za-z]([0-9A-Za-z]*)))){0,1}([+](([0-9A-Za-z]{1,})[.]){0,}([0-9A-Za-z]{1,})){0,1}$'
             if (!isRegexMatcher(regex, versionNum)) {
-                int index = 0
-                while (isRegexMatcher(regex, versionNum)) {  // 查询到符合语义化版本的Tag  防止tag不符合标准 导致生成的版本号无法连续 又重新1.0.0开始
-                    index++
+                int index = 1
+                while (!isRegexMatcher(regex, versionNum)) {  // 查询到符合语义化版本的Tag  防止tag不符合标准 导致生成的版本号无法连续 又重新1.0.0开始
                     versionNum = getShEchoResult(ctx, "git describe --abbrev=$index --tags")
+                    index++
                 }
-                ctx.println("循环查询到符合语义化版本的Tag为: " + versionNum)
             }
             def version = ""
             if (isRegexMatcher(regex, versionNum)) {
@@ -80,7 +79,7 @@ class Utils implements Serializable {
                 ctx.println("自动生成的语义化版本号v" + version + "已存在Git Tag中, 执行重新生成方法")
                 genSemverVersion(ctx, version, type)
             }
-
+            ctx.println("自动生成的语义化版本为: " + version)
         } catch (e) {
             return "1.0.0"
         }
