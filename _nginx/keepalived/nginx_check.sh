6884552e5971b233d5354fe5761d4cc25dbad680
panweiji <406798106@qq.com>
2023-05-17 13:02:43 +0800

    chore: Nginx负载均衡之Keepalived高可用

--- _nginx/keepalived/nginx_check.sh
+++ _nginx/keepalived/nginx_check.sh
@@ -2,7 +2,7 @@
 # Author: 潘维吉
 # Description: 定时检测Nginx的服务状态，如果Nginx停止，会尝试重新启动Nginx，如果启动失败，会将Keepalived服务停止，使IP漂移到备用节点上
 
-# 检测nginx是否启动  容器化检测设置  pidof nginx
+# 检测nginx是否启动  容器化检测设置  pidof nginx   多个节点的Nginx同时启动着
 pids=$(pidof nginx)
 
 if [[ ! ${pids} ]]; then
