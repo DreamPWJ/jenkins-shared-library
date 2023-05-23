573a554e37ef31d4e77e7f012b63d112e5366803
panweiji <406798106@qq.com>
2023-05-23 17:07:38 +0800

    chore: 基于QPS部署K8S弹性扩缩容

--- src/shared/library/Utils.groovy
+++ src/shared/library/Utils.groovy
@@ -49,8 +49,8 @@ class Utils implements Serializable {
         try {
             versionNum = versionNum.replaceAll("v", "").replaceAll("V", "") // 去掉前缀
             def regex = '^(([0-9]|([1-9]([0-9]*))).){2}([0-9]|([1-9]([0-9]*)))([-](([0-9A-Za-z]|([1-9A-Za-z]([0-9A-Za-z]*)))[.]){0,}([0-9A-Za-z]|([1-9A-Za-z]([0-9A-Za-z]*)))){0,1}([+](([0-9A-Za-z]{1,})[.]){0,}([0-9A-Za-z]{1,})){0,1}$'
-            if (!isRegexMatcher(regex, versionNum)) {
-                ctx.timeout(time: 1, unit: 'MINUTES') {
+            // if (!isRegexMatcher(regex, versionNum)) {
+                ctx.timeout(time: 2, unit: 'MINUTES') {
                     // 查询到符合语义化版本的Tag  防止tag不符合标准 导致生成的版本号无法连续 又重新1.0.0开始
                     def versionNumArray = getShEchoResult(ctx, "git tag") as ArrayList
                     for (int i = 0; i < versionNumArray.size(); i++) {
@@ -59,7 +59,7 @@ class Utils implements Serializable {
                         }
                     }
                 }
-            }
+           // }
             def version = ""
             if (isRegexMatcher(regex, versionNum)) {
                 version = versionNum.split("\\.")
