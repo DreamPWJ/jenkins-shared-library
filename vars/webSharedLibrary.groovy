24b57531ab7a748b2e5321264c2d458ffa86eeba
panweiji <406798106@qq.com>
2023-07-03 10:57:57 +0800

    feat: 完善Flutter For Web项目流水线

--- vars/webSharedLibrary.groovy
+++ vars/webSharedLibrary.groovy
@@ -315,7 +315,8 @@ def call(String type = 'web', Map map) {
                          }*/
                     tools {
                         // 工具名称必须在Jenkins 管理Jenkins → 全局工具配置中预配置 自动添加到PATH变量中
-                        nodejs "${NODE_VERSION}"
+                        // nodejs "${NODE_VERSION}"
+                        image "panweiji/node:${NODE_VERSION.replace('Node', '')}" // 为了更通用应使用通用镜像  自定义镜像针对定制化需求
                     }
                     steps {
                         script {
