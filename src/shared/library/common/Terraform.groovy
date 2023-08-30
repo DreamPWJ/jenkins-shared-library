ccd68d13e5f9961b90c460190b322777aa0546cc
panweiji <406798106@qq.com>
2023-08-30 12:48:55 +0800

    chore: Terraform基础设施即代码

--- src/shared/library/common/Terraform.groovy
+++ src/shared/library/common/Terraform.groovy
@@ -15,7 +15,14 @@ class Terraform implements Serializable {
      * 初始化
      */
     static def init(ctx) {
-        ctx.sh " "
+        // 安装
+        ctx.sh """
+        wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+        echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com \$(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+        sudo apt update && sudo apt install terraform
+        """
+        // 初始化
+        ctx.sh " terraform init "
     }
 
 }
