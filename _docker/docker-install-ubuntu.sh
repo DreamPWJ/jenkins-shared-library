0e61646c26a98729f173d61f31cb04f61b751cd7
panweiji <406798106@qq.com>
2023-11-15 16:57:04 +0800

    chore: 完善增强CI/CD基础架构

--- _docker/docker-install-ubuntu.sh
+++ _docker/docker-install-ubuntu.sh
@@ -2,6 +2,7 @@
 # Author: 潘维吉
 
 echo -e "\033[32mUbuntu系统Docker初始化安装  📥 \033[0m"
+# chmod +x docker-install-ubuntu.sh　给shell脚本执行文件可执行权限
 
 if [[ $(command -v docker) ]]; then
   echo -e "\033[34mDocker版本： $(docker --version) ，已经初始化 退出安装  ✘ \033[0m"
