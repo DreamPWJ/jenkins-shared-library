6b37ed58925351b9ccc1c8d5c30dfdd7bc8cb0fd
panweiji <406798106@qq.com>
2023-11-21 15:23:14 +0800

    chore: 完善增强CI/CD基础架构

--- _linux/shell/rsync.sh
+++ _linux/shell/rsync.sh
@@ -7,12 +7,12 @@ sudo apt-get install -y rsync tmux
 # sudo yum install -y rsync tmux
 
 
-# 基本示例 -r表示递归，即包含子目录  source表示源目录 destination表示目标目录
-rsync -r source destination
+# 基本示例 -r表示递归，即包含子目录  source表示源目录 target表示目标目录
+rsync -r /source/ /target/
 
 # 全量将远程内容同步到本地
-rsync -avP username@remote_host:source/ destination
+rsync -avP root@119.188.90.222:/mnt/source /mnt/
 
 # 增量同步  rsync 的最大特点就是它可以完成增量备份，也就是默认只复制有变动的文件 rsync命令会先扫描源路径，所以即使增量数据不多，也可能需要较长的时间完成
 # -delete参数删除只存在于目标目录、不存在于源目标的文件，即保证目标目录是源目标的镜像
-rsync -avP --delete  /source/path /target/path
+rsync -avP --delete  root@119.188.90.222:/mnt/ /mnt/
