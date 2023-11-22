0db0ffdd08a1dce052322d8fb09bdf3e60b61908
panweiji <406798106@qq.com>
2023-11-22 10:32:37 +0800

    chore: 完善增强CI/CD基础架构

--- _linux/shell/lvm-init.sh
+++ _linux/shell/lvm-init.sh
@@ -20,7 +20,7 @@ vgcreate vg_data /dev/sdb1
 vgdisplay
 
 # 新建一个LVM逻辑卷  lvdisplay 查看所有逻辑卷信息 -L 参数小于存储容量
-lvcreate -L 2026G -n lv_data vg_data
+lvcreate -L 299G -n lv_data vg_data
 lvdisplay
 
 # 格式化分区
@@ -29,15 +29,15 @@ fdisk -l
 
 # 挂载分区
 mkdir /mnt/data
-mount /dev/vg_data/lv_data /mnt/nfs_data
+mount /dev/vg_data/lv_data /tidb-data
 
 # 挂载永久生效  在 vim /etc/fstab内保存 重启等永久有效!!!
-# /dev/mapper/vg_data-lv_data /mnt/nfs_data xfs defaults 0 0
+# /dev/mapper/vg_data-lv_data /tidb-data xfs defaults 0 0
 vim /etc/fstab
 systemctl daemon-reload
 
 # 卸载分区
-umount /mnt/data
+umount /dev/vg_data/lv_data
 
 
 # 在线扩容硬盘  重新挂载新磁盘
@@ -45,6 +45,6 @@ lsblk
 fdisk /dev/sdc # fdisk分区 分别选m n p t(t代表LVM分区表 code设置8e) p w
 pvcreate /dev/sdc1 # 创建新物理卷
 vgextend vg_data /dev/sdc1 # 扩展VG到新磁盘
-lvextend -L +2046G /dev/vg_data/lv_data # 扩容逻辑卷  -L 参数小于分区存储容量
+lvextend -L +299G /dev/vg_data/lv_data # 扩容逻辑卷  -L 参数小于分区存储容量
 xfs_growfs /dev/vg_data/lv_data # 扩容文件系统
 df -h  # 查看扩容后分区大小
