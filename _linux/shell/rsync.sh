381b16658baf86a1ccef5a053605bc8eadf550cb
panweiji <406798106@qq.com>
2023-11-24 08:34:33 +0800

    chore: 完善增强CI/CD基础架构

--- _linux/shell/rsync.sh
+++ _linux/shell/rsync.sh
@@ -5,13 +5,13 @@
 # 安装迁移工具 传输的双方都必须安装rsync  rsync是负责执行复制的工具  tmux是帮助查看进度的工具
 sudo apt-get install -y rsync tmux
 # sudo yum install -y rsync tmux
-
+rsync --version
 
 # 基本示例 -r表示递归，即包含子目录  source表示源目录 target表示目标目录
 rsync -r /source/ /target/
 
-# 全量将远程内容同步到本地  --bwlimit=1000 限速1000KB/s
-rsync -avzP --progress --bwlimit=5120 root@119.188.90.222:/nfsdata/ParkPicture/stor1/2023/ /mnt/nfs_data/ParkPicture/stor1/2023/
+# 全量将远程内容同步到本地  --bwlimit=1000 限速单位KB/s  nohup输入密码后按ctrl+z 中断进程 紧接着输入bg后台运行 tail -f nohup.out
+nohup rsync -avzP --bwlimit=5120 root@119.188.90.222:/nfsdata/ParkPicture/stor1/2023/ /mnt/nfs_data/ParkPicture/stor1/2023/
 
 # 增量同步  rsync 的最大特点就是它可以完成增量备份，也就是默认只复制有变动的文件 rsync命令会先扫描源路径，所以即使增量数据不多，也可能需要较长的时间完成
 # -delete参数删除只存在于目标目录、不存在于源目标的文件，即保证目标目录是源目标的镜像
