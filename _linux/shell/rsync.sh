61620d35d3784e0be7617af2de48e642e252b343
panweiji <406798106@qq.com>
2023-11-27 11:28:52 +0800

    chore: 完善增强CI/CD基础架构

--- _linux/shell/rsync.sh
+++ _linux/shell/rsync.sh
@@ -12,7 +12,7 @@ rsync -r /source/ /target/
 
 # 全量将远程内容同步到本地  --bwlimit=1000 限速单位KB/s  --partial允许恢复中断的传输
 # nohup输入密码后按ctrl+z 中断进程 紧接着输入bg后台运行(需要实现ssh免密登录才不会中断) tail -f nohup.out
-nohup rsync -avzP --bwlimit=5120 root@119.188.90.222:/nfsdata/ParkPicture/stor1/2023/ /mnt/nfs_data/ParkPicture/stor1/2023/
+nohup rsync -avzP --bwlimit=5120 --exclude "1" root@119.188.90.222:/nfsdata/ParkPicture/stor1/2023/ /mnt/nfs_data/ParkPicture/stor1/2023/
 
 # 增量同步  rsync 的最大特点就是它可以完成增量备份，也就是默认只复制有变动的文件 rsync命令会先扫描源路径，所以即使增量数据不多，也可能需要较长的时间完成
 # -delete参数删除只存在于目标目录、不存在于源目标的文件，即保证目标目录是源目标的镜像
