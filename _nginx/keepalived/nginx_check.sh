940d329e81f18c950a6e773bcda22caacc10e9fc
panweiji <406798106@qq.com>
2023-11-29 14:00:56 +0800

    chore: 完善增强CI/CD基础架构

--- _nginx/keepalived/nginx_check.sh
+++ _nginx/keepalived/nginx_check.sh
@@ -2,6 +2,7 @@
 # Author: 潘维吉
 # Description: 定时检测Nginx的服务状态，如果Nginx停止，会尝试重新启动Nginx，如果启动失败，会将Keepalived服务停止，使IP漂移到备用节点上
 # 检测nginx是否启动  容器化检测设置  pidof nginx   多个节点的Nginx服务同时启动着
+# 执行授权  chmod 755 nginx_check.sh
 
 # 检测服务是否正常或存在  0 不正常不存在
 check_res=$(ps -C nginx --no-header | wc -l)
