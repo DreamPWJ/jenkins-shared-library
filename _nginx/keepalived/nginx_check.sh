30b1562712175c7e6ae4d88e3eb816671f4a3ecc
panweiji <406798106@qq.com>
2023-12-05 11:49:24 +0800

    chore: 完善增强CI/CD基础架构

--- _nginx/keepalived/nginx_check.sh
+++ _nginx/keepalived/nginx_check.sh
@@ -2,7 +2,7 @@
 # Author: 潘维吉
 # Description: 定时检测Nginx的服务状态，如果Nginx停止，会尝试重新启动Nginx，如果启动失败，会将Keepalived服务停止，使IP漂移到备用节点上
 # 检测nginx是否启动  容器化检测设置  pidof nginx   多个节点的Nginx服务同时启动着
-# 执行授权  chmod 755 nginx_check.sh
+# chmod 755 /etc/keepalived/nginx_check.sh &&  systemctl restart  keepalived  给shell脚本执行文件可执行权限
 
 # 检测服务是否正常或存在  0 不正常不存在
 check_res=$(ps -C nginx --no-header | wc -l)
@@ -13,8 +13,8 @@ if [[ check_res -eq 0 ]]; then
    # /usr/sbin/nginx
    # systemctl start nginx
    # Docker容器启动
-   docker restart proxy-nginx
-   sleep 2
+   # docker restart proxy-nginx
+   # sleep 2
    check_res2=$(ps -C nginx --no-header | wc -l)
   if [[ check_res2 -eq 0 ]]; then
     # /usr/sbin/keepalived -s stop
@@ -22,8 +22,15 @@ if [[ check_res -eq 0 ]]; then
     systemctl stop keepalived
     # killall keepalived
   fi
+#  else
+#   # 如果keepalived服务不存在 启动keepalived服务  因为当前keepalived已停止无法再检测
+#   keepalived_check_res=$(ps -C keepalived --no-header | wc -l)
+#   if [[ keepalived_check_res -eq 0 ]]; then
+#      echo $(date)  "keepalived is not running, start..." >> /etc/keepalived/check_nginx.log
+#      systemctl start keepalived
+#      # systemctl status keepalived
+#   fi
 fi
-
 
 # 创建定时任务 检测nginx是否存活来控制keepalived切换  ！！！在keepalived.conf已经配置了检测机制 无需再做定时任务
 # sudo crontab -e
