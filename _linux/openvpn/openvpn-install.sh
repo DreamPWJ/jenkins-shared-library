3ba1bec5b2f33346d9b21783447e9634fa48e142
panweiji <406798106@qq.com>
2023-12-14 12:35:51 +0800

    chore: 完善增强CI/CD基础架构

--- _linux/openvpn/openvpn-install.sh
+++ _linux/openvpn/openvpn-install.sh
@@ -7,6 +7,7 @@
 # VPN证书在root目录下  .ovpn格式  直接给客户端导入即可
 # 在服务端设置生成客户端ovpn证书的有效期  vim /etc/openvpn/server/easy-rsa/pki/vars 中 set_var EASYRSA_CERT_EXPIRE	3650  默认825
 # 测试UDP服务连通性  nc -vuz 172.16.100.177 19271
+# 设置版本号  sudo apt-get install -y openvpn=2.4.12
 
 # Detect Debian users running the script with "sh" instead of bash
 if readlink /proc/$$/exe | grep -q "dash"; then
