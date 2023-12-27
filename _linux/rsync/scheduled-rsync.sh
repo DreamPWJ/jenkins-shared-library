98ae0197f3903f64e54d1e7b25b15defb0a5e227
panweiji <406798106@qq.com>
2023-12-27 09:38:37 +0800

    chore: 完善增强CI/CD基础架构

--- _linux/rsync/scheduled-rsync.sh
+++ _linux/rsync/scheduled-rsync.sh
@@ -11,11 +11,13 @@ sleep 60
 nohup rsync -avzP --delete --bwlimit=5120 root@172.16.100.185:/my/backup /tidb-data/ > nohup.out 2>&1 &
 
 
+chmod -R 777 /tidb-data/backup
+
 # 创建定时任务 
 # chmod +x scheduled-rsync.sh  给shell脚本执行文件可执行权限
 # sudo crontab -e
 # 每月执行一次 0 0 1 * *  每天凌晨2点执行一次 0 2 * * *
-# 0 1 * * * /bin/bash /my/scheduled-rsync.sh >/my/rsync-crontab.log 2>&1
+# 0 2 * * * /bin/bash /my/scheduled-rsync.sh >/my/rsync-crontab.log 2>&1
 # service crond restart , Ubuntu 使用 sudo service cron start # 重启crond生效
 # crontab -l # 查看crond列表
 # GNU nano编辑器CTRL+O 再 CTRL+X 保存退出
