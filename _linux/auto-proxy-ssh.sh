8858845ef570a2cbb8034ed56a3945f48e6a8954
panweiji <406798106@qq.com>
2024-01-22 17:05:39 +0800

    chore: 完善增强CI/CD基础架构

--- _linux/auto-proxy-ssh.sh
+++ _linux/auto-proxy-ssh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Author: 潘维吉
-# Description:  跳板机方式批量执行SSH免密登录    chmod +x auto-proxy-ssh.sh  在hosts.txt内批量设置机器的ip 用户名 密码
+# Description:  跳板机方式自动批量执行SSH ProxyCommand免密登录 chmod +x auto-proxy-ssh.sh  在proxy_jump_hosts.json内批量设置机器的ip 用户名 密码
 # !!!注意当前机器先执行 ssh-keygen -t rsa
 # 安全性高和定制化的数据建议保存为Jenkins的“Secret file”类型的凭据并获取 无需放在代码中
 
@@ -9,6 +9,8 @@
 # 2. 跳板机再免密到目标机 同理1
 # 3. 最后将客户端的公钥 cat /root/.ssh/id_rsa.pub 放到内网目标机 vim /root/.ssh/authorized_keys 授信  systemctl restart sshd
 # 在执行SSH跳板命令生效:  ssh -J root@外网跳板机IP:22 root@内网目标机器IP -p 22
+
+echo "跳板机方式自动批量执行SSH ProxyCommand免密登录"
 
 if [[ ! $(command -v jq) ]]; then
 sudo apt install -y jq || true
@@ -23,51 +25,42 @@ json_file="proxy_jump_hosts.json"
 
 # 如果是从文件读取则用 cat json_file | jq ...
 # 对于变量中的JSON数据，直接通过echo传递给jq
+
 while read host; do
     jump_host=$(echo "$host" | jq -r '.jump_host')
+    jump_user_name=$(echo "$host" | jq -r '.jump_user_name')
+    jump_password=$(echo "$host" | jq -r '.jump_password')
+    jump_port=$(echo "$host" | jq -r '.jump_port')
     echo "jump_host: $jump_host"
+  expect <<EOF
+        spawn ssh-copy-id -i $HOME/.ssh/id_rsa.pub  $jump_user_name@$jump_host
+        expect {
+                "yes/no" {send "yes\n";exp_continue}
+                "password" {send "$jump_password\n"}
+        }
+        expect eof
+EOF
 
+  # 跳板机登录目标主机
   while read item_host; do
        # echo "item_host: $item_host"
         target_host=$(echo "$item_host" | jq -r '.target_host')
         target_user_name=$(echo "$item_host" | jq -r '.target_user_name')
+        target_password=$(echo "$item_host" | jq -r '.target_password')
+        target_port=$(echo "$item_host" | jq -r '.target_port')
         echo "target_host: $target_host ,  target_user_name: $target_user_name"
-    done <<< "$(echo "$host" | jq -c '.target_hosts[]')"
+
+        # 通过跳板机登录目标主机 ssh -J root@外网跳板机IP:22 root@内网目标机器IP -p 22 '命令'
+        # spawn ssh-copy-id -i $HOME/.ssh/id_rsa.pub  $target_user_name@$target_host
+  expect <<EOF
+        spawn ssh -J $jump_user_name@$jump_host:$jump_port $target_user_name@$target_host -p $target_port 'ssh-copy-id -i $HOME/.ssh/id_rsa.pub'
+        expect {
+                "yes/no" {send "yes\n";exp_continue}
+                "password" {send "$target_password\n"}
+        }
+        expect eof
+EOF
+  done <<< "$(echo "$host" | jq -c '.target_hosts[]')"
 
 done  <<< "$(cat "$json_file" | jq -c '.[]')"
 
-# 跳板机地址
-#set jump_host "jump_host_address"
-#
-## 用户名和密码
-#set username "your_username"
-#set password "your_password"
-#
-## 目标主机列表
-#set hosts_list [list "host1" "host2" "host3"]
-#
-#foreach host $hosts_list {
-#    # 通过跳板机登录目标主机
-#    spawn ssh -o ProxyCommand="ssh -W %h:%p $username@$jump_host" $username@$host
-#
-#    # 期望交互式输入并发送密码
-#    expect "*assword:*"
-#    send "$password\r"
-#
-#    # 发送一个简单的命令以确认连接成功（例如：ls）
-#    expect "#"
-#    send "ls\r"
-#    # 钥添加到了目标主机的~/.ssh/authorized_keys文件中以实现免密登
-#    send "ssh-copy-id -i $HOME/.ssh/id_rsa.pub  $username@$host\r"
-#    # 等待命令执行完成
-#    expect "#"
-#
-#    # 打印分隔符，便于区分不同主机的结果
-#    puts "\n--- Connection to $host ---\n"
-#
-#    # 关闭当前连接
-#    send "exit\r"
-#    expect eof
-#}
-#
-#puts "\nAll connections completed."
