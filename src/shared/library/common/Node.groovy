d993064fa62aae48e2fd6f94155873796183a8c8
panweiji <406798106@qq.com>
2024-01-23 10:01:16 +0800

    chore: 完善增强CI/CD基础架构

--- src/shared/library/common/Node.groovy
+++ src/shared/library/common/Node.groovy
@@ -41,8 +41,8 @@ class Node implements Serializable {
         }
 
         // 设置镜像源 加速下载
-        ctx.sh "npm config set registry https://registry.npm.taobao.org"
-        ctx.sh "yarn config set registry https://registry.npm.taobao.org"
+        ctx.sh "npm config set registry http://registry.npm.taobao.org"
+        ctx.sh "yarn config set registry http://registry.npm.taobao.org"
         ctx.sh "npm config set sass_binary_site https://npm.taobao.org/mirrors/node-sass/"
         ctx.sh "npm config set electron_mirror https://npm.taobao.org/mirrors/electron/"
         ctx.sh "yarn config set electron_mirror https://npm.taobao.org/mirrors/electron/"
@@ -59,8 +59,8 @@ class Node implements Serializable {
         } catch (error) {
             ctx.sh "npm install -g yarn"  // 动态配置或固定yarn版本号 防止版本变化兼容性问题
         }
-        ctx.sh "npm config set registry https://registry.npm.taobao.org"
-        ctx.sh "yarn config set registry https://registry.npm.taobao.org"
+        ctx.sh "npm config set registry http://registry.npm.taobao.org"
+        ctx.sh "yarn config set registry http://registry.npm.taobao.org"
         ctx.sh "npm config set electron_mirror https://npm.taobao.org/mirrors/electron/"
         ctx.sh "yarn config set electron_mirror https://npm.taobao.org/mirrors/electron/"
         // ctx.sh "yarn config set electron_mirror https://mirrors.huaweicloud.com/electron/"
