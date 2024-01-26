636e954702a06649b855fffe5aae5f4e2f6a4310
panweiji <406798106@qq.com>
2024-01-26 16:49:42 +0800

    chore: 完善增强CI/CD基础架构

--- vars/sharedLibrary.groovy
+++ vars/sharedLibrary.groovy
@@ -1023,6 +1023,7 @@ def pullProjectCode() {
         println "Git构建分支是: ${BRANCH_NAME} 📇"
         // def git = git url: "${REPO_URL}", branch: "${BRANCH_NAME}", credentialsId: "${GIT_CREDENTIALS_ID}"
         // println "${git}"
+        sh "git --version"
         // 对于大体积仓库或网络不好情况 自定义代码下载超时时间 默认10分钟
         checkout([$class           : 'GitSCM',
                   branches         : [[name: "*/${BRANCH_NAME}"]],
