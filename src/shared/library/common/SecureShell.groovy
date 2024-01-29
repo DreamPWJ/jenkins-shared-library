58ea1b6da905bd28e412e242e1177e209c16c8ff
panweiji <406798106@qq.com>
2024-01-29 13:16:30 +0800

    -

--- src/shared/library/common/SecureShell.groovy
+++ src/shared/library/common/SecureShell.groovy
@@ -1,5 +1,6 @@
 package shared.library.common
 
+import groovy.json.JsonOutput
 import shared.library.Utils
 
 /**
@@ -62,8 +63,10 @@ class SecureShell implements Serializable {
                                 def jsonData = ctx.readFile(file: "${ctx.PROXY_JUMP_HOSTS}")
                                 def json = ctx.readJSON text: "${jsonData}"
                                 def filePath = "_linux/proxy_jump_hosts.json"
-                                // 使用 Groovy 代码写入文件   .join('\n') 将ArrayList的内容合并成一个多行字符串
-                                ctx.writeFile file: filePath, text: "[" + json.join('\n') + "]"
+                                // 使用 Groovy 代码写入文件
+                                // 将数组转换为JSON格式的字符串
+                                def jsonText = JsonOutput.toJson(json)
+                                ctx.writeFile file: filePath, text: jsonText
                             }
                         }
 
