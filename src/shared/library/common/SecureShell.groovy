85f0b7c7593abc3205f610a9918d054ae4ffc5c8
panweiji <406798106@qq.com>
2024-01-29 12:48:28 +0800

    -

--- src/shared/library/common/SecureShell.groovy
+++ src/shared/library/common/SecureShell.groovy
@@ -29,7 +29,7 @@ class SecureShell implements Serializable {
 
             if ("${ctx.isProxyJumpType}" == "true") {  // 跳板机方式部署
                 // 执行升级检测  在较新版本的OpenSSH 7.3及以上中( ssh -V 查看版本)，跳板机（jump host）-J 选项是存在的
-                ctx.sh " cd _linux/shell/ && chmod +x upgrade-ssh.sh && ./upgrade-ssh.sh "
+                ctx.sh " cd ci/_linux/shell/ && chmod +x upgrade-ssh.sh && ./upgrade-ssh.sh "
             }
 
             // 检测ssh免密连接是否成功 ssh/scp跳过首次连接远程主机的指纹fingerprint(防止中间人攻击)设置-o StrictHostKeyChecking=no
@@ -37,8 +37,7 @@ class SecureShell implements Serializable {
         } catch (error) {
             ctx.println error.getMessage()
             if (error.getMessage().contains("255")) { // 0连接成功 255无法连接
-                ctx.println "免密登录失败, 根据hosts.txt文件已有的账号信息自动设置, 如果没有配置hosts.txt请手动设置ssh免密登录"
-                ctx.println "如果有跳板机情况, 可手动将构建机器的公钥分别添加到外网跳板机和内网目标机authorized_keys内实现免密登录"
+                ctx.println "免密登录失败 ❌, 根据hosts.txt或proxy_jump_hosts.json文件已有的账号信息自动设置, 如果没有配置请手动设置ssh免密登录文件"
                 try {
                     // 目的是清除当前机器里关于远程服务器的缓存和公钥信息 如远程服务器已重新初始化或升降配等情况 导致本地还有缓存
                     // ECDSA host key "ip" for  has changed and you have requested strict checking 报错
@@ -58,7 +57,7 @@ class SecureShell implements Serializable {
                                 ctx.writeFile file: filePath, text: textData
                             }
                         }
-                        if ("${map.proxy_jump_hosts_id}".trim() != "") {
+                        if ("${ctx.isProxyJumpType}" == "true") {
                             ctx.withCredentials([ctx.file(credentialsId: "${map.proxy_jump_hosts_id}", variable: 'PROXY_JUMP_HOSTS')]) {
                                 def jsonData = ctx.readFile(file: "${ctx.PROXY_JUMP_HOSTS}")
                                 def json = ctx.readJSON text: "${jsonData}"
@@ -67,10 +66,14 @@ class SecureShell implements Serializable {
                                 ctx.writeFile file: filePath, text: json
                             }
                         }
-                        // 执行免密登录脚本
-                        ctx.sh " cd _linux && chmod +x auto-ssh.sh && ./auto-ssh.sh "
-                        // 执行跳板机方式免密登录脚本
-                        ctx.sh " cd _linux && chmod +x auto-proxy-ssh.sh && ./auto-proxy-ssh.sh "
+
+                        if ("${ctx.isProxyJumpType}" == "true") {
+                            // 执行跳板机方式免密登录脚本
+                            ctx.sh " cd _linux && chmod +x auto-proxy-ssh.sh && ./auto-proxy-ssh.sh "
+                        } else {
+                            // 执行免密登录脚本
+                            ctx.sh " cd _linux && chmod +x auto-ssh.sh && ./auto-ssh.sh "
+                        }
                     } catch (e) {
                         ctx.println e.getMessage()
                     }
