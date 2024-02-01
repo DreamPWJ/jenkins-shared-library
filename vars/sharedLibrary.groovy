5c752387651af190854f8c759018b528ba80632c
panweiji <406798106@qq.com>
2024-02-01 14:55:51 +0800

    chore: 完善增强CI/CD基础架构

--- vars/sharedLibrary.groovy
+++ vars/sharedLibrary.groovy
@@ -1160,7 +1160,7 @@ def nodeBuildProject() {
                         retry(3) {
                             retryCount++
                             if (retryCount >= 2) {
-                                sh "rm -rf node_modules && rm -f *.lock.json"
+                                sh "rm -rf node_modules && rm -f *.lock.*"
                                 // 如果包404下载失败  可以更换官方镜像源重新下载
                                 Node.setOfficialMirror(this)
                             }
@@ -1186,7 +1186,7 @@ def nodeBuildProject() {
                     }
                 } catch (e) {
                     println(e.getMessage())
-                    sh "rm -rf node_modules && rm -f *.lock.json"
+                    sh "rm -rf node_modules && rm -f *.lock.*"
                     error("Web打包失败, 终止当前Pipeline运行 ❌")
                 }
             }
