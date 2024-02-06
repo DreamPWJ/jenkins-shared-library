b09dcbd566a900916164cf924ebc46ea5efc8779
panweiji <406798106@qq.com>
2024-02-06 16:24:43 +0800

    chore: 完善增强CI/CD基础架构

--- src/shared/library/common/Kubernetes.groovy
+++ src/shared/library/common/Kubernetes.groovy
@@ -264,7 +264,9 @@ class Kubernetes implements Serializable {
         // 等待所有Pod达到Ready状态
         ctx.timeout(time: 10, unit: 'MINUTES') { // 设置超时时间
             def podsAreReady = false
+            def whileCount = 0  // 循环次数
             while (!podsAreReady) {
+                whileCount++
                 def output = ctx.sh(script: "kubectl get pods -n $namespace -l app=$deploymentName -o json", returnStdout: true)
                 def podStatus = ctx.readJSON text: output
 
@@ -284,12 +286,17 @@ class Kubernetes implements Serializable {
                     }
                 }
             }
+
             // 除了Running之外的状态  都不能算部署成功 Pod处于Pending状态也会通过上面的Ready状态检测代码 其实部署是失败的
             // 如Pending由于资源不足或其他限制  Terminating器可能还在停止中或资源清理阶段  ContainerCreating 容器尚未创建完成
             // Failed 如果Pod中的所有容器都因失败而退出，并且不会再重启，则Pod会进入Failed状态  CrashLoopBackOff 时，这意味着 Pod 中的主容器（或其中一个容器）反复启动并快速退出
             // 示例 查询pod所有节点的状态  kubectl get pods --selector=app=my-app -o=jsonpath='{range .items[*]}{.metadata.name}{"\t"}{.status.phase}{"\n"}{end}'
-
-            Tools.printColor(ctx, "K8S集群中所有Pod服务已处于启动状态 ✅")
+            if (whileCount == 1) { // 只执行一次的话 健康探测失败
+                Tools.printColor(ctx, "K8S集群中Pod服务部署启动失败  ❌", "red")
+                ctx.error("K8S集群中Pod服务部署启动失败 ❌")
+            } else {
+                Tools.printColor(ctx, "K8S集群中所有Pod服务已处于启动状态 ✅")
+            }
         }
 
     }
