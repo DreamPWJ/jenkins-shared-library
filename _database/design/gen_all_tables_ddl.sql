8020b06b480a7a740fd7a9f56931e94e280d5816
panweiji <406798106@qq.com>
2024-05-23 11:49:03 +0800

    chore: 完善增强CI/CD基础架构

--- _database/design/gen_all_tables_ddl.sql
+++ _database/design/gen_all_tables_ddl.sql
@@ -7,4 +7,4 @@ WHERE table_schema = '你的数据库名';
 SHOW CREATE TABLE `user`;
 
 # 导出全库DDL  定时提交到Git版本控制  用于数据库版本控制
-# mysql -u 用户名 -p 密码  你的数据库名 < /tmp/show_create_tables.sql > /tmp/all_tables_ddl.sql
+# mysql -u 用户名 -p 密码  你的数据库名 < /tmp/show_create_tables.sql > /tmp/gen_all_tables_ddl.sql
