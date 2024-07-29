af4224a1ea44c90dfc0f381910f5802432792fa5
panweiji <406798106@qq.com>
2024-07-29 09:26:37 +0800

    chore: 强大灵活易用的多端CI/CD共享库流水线

--- _database/design/scripts/db_health_check.sh
+++ _database/design/scripts/db_health_check.sh
@@ -4,9 +4,10 @@
 
 # MySQL服务器配置
 DB_HOST="localhost"
+DB_PORT="3306"
 DB_USER="root"
 DB_PASS="password"
-DB_NAME="mydatabase"
+DB_NAME="database_name"
 
 # 检查数据库服务是否运行
 if ! systemctl is-active mysql.service > /dev/null 2>&1; then
@@ -17,26 +18,26 @@ fi
 
 # 尝试连接数据库并执行简单查询
 QUERY="SELECT 1"
-if ! mysql -h $DB_HOST -u $DB_USER -p$DB_PASS -e "$QUERY" < /dev/null; then
+if ! mysql -h $DB_HOST -P $DB_PORT -u $DB_USER -p$DB_PASS -e "$QUERY" < /dev/null; then
     echo "无法连接到数据库"
     systemctl restart mysql.service
     exit 1
 fi
 
 # 检查表空间
-TABLESPACE_QUERY="SHOW TABLE STATUS WHERE Engine='InnoDB'"
-tablespaces=$(mysql -h $DB_HOST -u $DB_USER -p$DB_PASS $DB_NAME -e "$TABLESPACE_QUERY")
-if [[ $tablespaces == *"Data_free"* ]]; then
-    echo "存在数据空间碎片"
-    exit 1
-fi
+# TABLESPACE_QUERY="SHOW TABLE STATUS WHERE Engine='InnoDB'"
+# tablespaces=$(mysql -h $DB_HOST -P $DB_PORT -u $DB_USER -p$DB_PASS $DB_NAME -e "$TABLESPACE_QUERY")
+# if [[ $tablespaces == *"Data_free"* ]]; then
+#     echo "存在数据空间碎片"
+#     exit 1
+# fi
 
 # 检查慢查询
-SLOW_QUERY_LOG="/var/log/mysql/slowquery.log"
-if [ -s $SLOW_QUERY_LOG ]; then
-    echo "检测到慢查询"
-    exit 1
-fi
+# SLOW_QUERY_LOG="/var/log/mysql/slowquery.log"
+# if [ -s $SLOW_QUERY_LOG ]; then
+#     echo "检测到慢查询"
+#     exit 1
+# fi
 
 echo "数据库健康检查通过"
 exit 0
@@ -45,6 +46,6 @@ exit 0
 
 # 执行授权  chmod +x /my/db_health_check.sh
 # crontab -e
-# */5 * * * * /bin/bash /my/db_health_check.sh
+# */5 * * * * /bin/bash /my/db_health_check.sh > /my/db-health-check.log 2>&1
 # service crond restart  , Ubuntu 使用 sudo service cron restart # 重启crond生效
 # crontab -l # 查看crond列表
