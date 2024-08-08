8d6a7c041543bf7f8039ecd98efc8488cc1964b5
panweiji <406798106@qq.com>
2024-08-08 15:39:44 +0800

    chore: 强大灵活易用的多端CI/CD共享库流水线

--- vars/sharedLibrary.groovy
+++ vars/sharedLibrary.groovy
@@ -44,7 +44,8 @@ def call(String type = 'web-java', Map map) {
             parameters {
                 choice(name: 'DEPLOY_MODE', choices: [GlobalVars.release, GlobalVars.rollback, GlobalVars.start, GlobalVars.stop, GlobalVars.restart],
                         description: '选择部署方式  1. ' + GlobalVars.release + '发布 2. ' + GlobalVars.rollback +
-                                '回滚(基于jenkins归档方式回滚选择' + GlobalVars.rollback + ', 基于Git Tag方式回滚请选择默认的' + GlobalVars.release + ')')
+                                '回滚(基于jenkins归档方式回滚选择' + GlobalVars.rollback + ', 基于Git Tag方式回滚请选择默认的' + GlobalVars.release + ') ' +
+                                ' 3. ' + GlobalVars.start + '启动服务 4. ' + GlobalVars.stop + '停止服务 5. ' + GlobalVars.restart + '重启服务')
                 choice(name: 'MONOREPO_PROJECT_NAME', choices: "${MONOREPO_PROJECT_NAMES}",
                         description: "选择MonoRepo单体式统一仓库项目名称, ${GlobalVars.defaultValue}选项是MultiRepo多体式独立仓库或未配置, 大统一单体式仓库流水线可减少构建时间和磁盘空间")
                 gitParameter(name: 'GIT_BRANCH', type: 'PT_BRANCH', defaultValue: "${BRANCH_NAME}", selectedValue: "DEFAULT",
@@ -655,6 +656,14 @@ def call(String type = 'web-java', Map map) {
                         beforeAgent true
                         expression {
                             return GlobalVars.start == params.DEPLOY_MODE || GlobalVars.start == params.DEPLOY_MODE || GlobalVars.restart == params.DEPLOY_MODE
+                        }
+                    }
+                    agent {
+                        docker {
+                            //   构建完成自动删除容器
+                            image "panweiji/k8s:latest"
+                            // args " "
+                            reuseNode true // 使用根节点
                         }
                     }
                     steps {
