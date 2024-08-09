4d02cef0ca665b34606cea3f46d987a7d9fc784e
panweiji <406798106@qq.com>
2024-08-09 09:17:01 +0800

    chore: 强大灵活易用的多端CI/CD共享库流水线

--- src/shared/library/common/Deploy.groovy
+++ src/shared/library/common/Deploy.groovy
@@ -169,17 +169,17 @@ class Deploy implements Serializable {
                 }
                 ctx.remote_worker_ips.each { ip ->
                     ctx.println ip
+                    ctx.sh " ssh ${ctx.proxyJumpSSHText} ${ctx.remote.user}@${ip} ' " + command + " ' "
                     if (GlobalVars.restart == ctx.params.DEPLOY_MODE) {
                         // ctx.sleep 30  // 重启多个服务 防止服务不可用等待顺序重启
                         // 健康检测  判断服务是否启动成功
                         ctx.timeout(time: 5, unit: 'MINUTES') {  // health-check.sh有检测超时时间 timeout为防止shell脚本超时失效兼容处理
-                           def healthCheckMsg = ctx.sh(
+                            def healthCheckMsg = ctx.sh(
                                     script: "ssh  ${ctx.proxyJumpSSHText} ${ctx.remote.user}@${ip} ' cd /${ctx.DEPLOY_FOLDER}/ && ./health-check.sh -a ${ctx.PROJECT_TYPE} -b http://${ip}:${ctx.SHELL_HOST_PORT} '",
                                     returnStdout: true).trim()
                             ctx.println "${healthCheckMsg}"
                         }
                     }
-                    ctx.sh " ssh ${ctx.proxyJumpSSHText} ${ctx.remote.user}@${ip} ' " + command + " ' "
                 }
             }
         }
