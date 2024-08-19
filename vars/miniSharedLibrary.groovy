ca1cde5d1c7b0e4fe3341022c88a3116a2b55997
panweiji <406798106@qq.com>
2024-08-19 17:27:55 +0800

    chore: 强大灵活易用的多端CI/CD共享库流水线

--- vars/miniSharedLibrary.groovy
+++ vars/miniSharedLibrary.groovy
@@ -778,7 +778,6 @@ def submitAudit() {
     try {
         timeout(time: 20, unit: 'MINUTES') { // 下载playwright支持的浏览器下载比较耗时
             docker.image("mcr.microsoft.com/playwright:v${playwrightVersion}-jammy").inside {
-                sh 'npx playwright test'
                 // sh "playwright --version"
                 PlayWright.miniPlatform(this)
             }
