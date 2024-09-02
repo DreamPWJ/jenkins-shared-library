b935f90ca7b7bca966d7a78306da4c0c40abc330
panweiji <406798106@qq.com>
2024-09-02 16:55:32 +0800

    chore: 强大灵活易用的多端CI/CD共享库流水线

--- _docker/docker-install-centos.sh
+++ _docker/docker-install-centos.sh
@@ -18,7 +18,7 @@ sudo yum update -y || true
 # 安装需要的软件包， yum-util 提供yum-config-manager功能，另外两个是devicemapper驱动依赖的
 sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 # 设置yum源 https://download.docker.com/linux/centos/docker-ce.repo
-#sudo yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+sudo yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
 
 echo "安装Docker"
 sudo yum makecache # 将服务器上的软件包信息 现在本地缓存,以提高 搜索 安装软件的速度
