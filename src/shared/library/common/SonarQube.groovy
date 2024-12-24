4e1830e946140dfb29dda450686da679916e414d
panweiji <406798106@qq.com>
2024-12-24 14:39:24 +0800

    chore: 强大灵活易用的多端CI/CD共享库流水线

--- src/shared/library/common/SonarQube.groovy
+++ src/shared/library/common/SonarQube.groovy
@@ -19,11 +19,15 @@ class SonarQube implements Serializable {
      * 创建sonar项目
      */
     static def createProject(ctx, projectName) {
-        ctx.println("CI自动创建SonarQube项目")
-        def apiUrl = "projects/create?name=${projectName}&project=${projectName}"
-        // 发请求
-        def response = httpReq(ctx, "POST", apiUrl, "")
-        ctx.println(response)
+        if (searchProject(ctx, projectName) == "false") {
+            ctx.println("CI自动创建SonarQube项目")
+            def apiUrl = "projects/create?name=${projectName}&project=${projectName}"
+            // 发请求
+            def response = doHttpRequest(ctx, "POST", apiUrl, "")
+            ctx.println(response)
+        } else {
+            ctx.println(projectName + "项目已存在  无需自动创建SonarQube项目")
+        }
     }
 
     /**
@@ -67,7 +71,7 @@ class SonarQube implements Serializable {
     static def getStatus(ctx, projectName) {
         def apiUrl = "${SonarQube.sonarServer}" + "/project_branches/list?project=${projectName}"
         // 发请求
-        def responses = HttpRequest.get(ctx, apiUrl)
+        def responses = ctx.HttpRequest("GET", apiUrl)
         ctx.println(responses)
         // 对返回的文本做JSON解析
         def response = ctx.readJSON text: "${responses}"
@@ -78,10 +82,27 @@ class SonarQube implements Serializable {
     }
 
     /**
+     * 搜索Sonar项目
+     */
+    static def searchProject(ctx, projectName) {
+        def apiUrl = "projects/search?projects=${projectName}"
+        def response = ctx.HttpRequest("GET", apiUrl, '')
+
+        response = ctx.readJSON text: """${response.content}"""
+        def result = response["paging"]["total"]
+
+        if (result.toString() == "0") {
+            return "false"
+        } else {
+            return "true"
+        }
+    }
+
+    /**
      * 封装HTTP请求
      * 或更复杂的需求可使用 OKHTTP库
      */
-    static def httpReq(ctx, requestType, requestUrl, requestBody) {
+    static def doHttpRequest(ctx, requestType, requestUrl, requestBody) {
         // 定义sonar api接口
         def sonarServerApi = "${sonarServer}/api"
         def result = ctx.httpRequest authentication: 'sonar-admin-user', // 在系统管理->系统配置->HTTP Request中配置 应用
