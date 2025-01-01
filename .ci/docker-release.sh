4f910cc7a47e9a17cde1cf922ece1ea76e91ce27
panweiji <406798106@qq.com>
2025-01-01 10:28:58 +0800

    chore: 强大灵活易用的多端CI/CD共享库流水线

--- .ci/docker-release.sh
+++ .ci/docker-release.sh
@@ -166,6 +166,8 @@ if [[ ${is_prod} == false ]]; then
   echo "逻辑CPU的个数: ${cup_num}"
   # limit_cup_num=$(echo "scale=4; ${cup_num} - 0.3" | bc) # 浮点数计算  bc命令可能不存在
   dynamic_run_args=${dynamic_run_args}" --cpus=${cup_num} "
+
+  # 限制资源 cpu_cores=$(nproc) cpus_limit=$(awk "BEGIN {print $cpu_cores * 0.8}")  --cpus=$cpus_limit 防止整个服务器资源被占用停机
 fi
 
 # 动态参数
@@ -248,7 +250,6 @@ fi
 
 echo -e "\033[32m 👨‍💻  启动运行Docker容器 环境: ${env_mode} 映射端口: ${host_port}:${expose_port} \033[0m"
 # --pid=host 使用宿主机命名空间 方便容器获取宿主机所有进程 解决多个docker节点RocketMQ重复消费消息等问题
-# 限制资源 --cpus=${docker_cpu} 防止整个服务器资源被占用停机
 docker run -d --restart=on-failure:16 -p ${host_port}:${expose_port} --privileged=true --pid=host \
   -e "SPRING_PROFILES_ACTIVE=${env_mode}" -e "PROJECT_NAME=${project_name}" -e "DOCKER_SERVICE_PORT=${build_expose_ports}" \
   -e "JAVA_OPTS=-Xms128m ${docker_java_opts}" -m ${docker_memory} --log-opt ${docker_log_opts} --log-opt max-file=1  ${dynamic_run_args} \
