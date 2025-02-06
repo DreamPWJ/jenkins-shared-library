499dd26a77b3d2a94811b073e93ec0df22470073
panweiji <406798106@qq.com>
2025-02-06 09:29:05 +0800

    feat: 强大灵活易用的多端CI/CD共享库流水线

--- src/shared/library/common/SecureShell.groovy
+++ src/shared/library/common/SecureShell.groovy
@@ -45,7 +45,7 @@ class SecureShell implements Serializable {
                     } else {
                         remoteHostIp = "${ctx.remote.host}"
                     }
-                    // 清除当前机器里关于远程服务器的缓存和公钥信息
+                    // 清除当前机器里关于远程服务器的缓存和公钥信息 可清空跳板机和服务器/root/.ssh/authorized_keys内的缓存数据
                     ctx.sh "ssh-keygen -R ${remoteHostIp}"
                     if ("${ctx.isProxyJumpType}" == "true") {
                         ctx.sh "ssh-keygen -f /root/.ssh/known_hosts -R ${ctx.remote.host}"
