3e09b08cea9cc4a1fc5779071e76d575d68126a5
panweiji <406798106@qq.com>
2025-02-07 14:38:48 +0800

    feat: 强大灵活易用的多端CI/CD共享库流水线

--- src/shared/library/common/SecureShell.groovy
+++ src/shared/library/common/SecureShell.groovy
@@ -47,14 +47,8 @@ class SecureShell implements Serializable {
                     }
                     // 清除当前机器里关于远程服务器的缓存和公钥信息 可清空跳板机和服务器/root/.ssh/authorized_keys内的缓存数据
                     ctx.sh "ssh-keygen -R ${remoteHostIp}"
-
-                    /*    try {
-                            // 刷新known_hosts中对应远程服务器公钥
-                            ctx.sh "ssh-keyscan  ${remoteHostIp} >> ~/.ssh/known_hosts"
-                        } catch (e) {
-                            // 删除known_hosts中对应远程服务器公钥 重新初始化
-                            ctx.sh "rm -f ~/.ssh/known_hosts"
-                        }*/
+                    // 删除known_hosts中对应远程服务器公钥 重新初始化
+                    // ctx.sh "rm -f ~/.ssh/known_hosts"
                 } catch (e) {
                     ctx.println "清除当前机器里关于远程服务器的缓存和公钥信息失败"
                     ctx.println e.getMessage()
