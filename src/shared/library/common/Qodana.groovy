e1e6219ddf0013aad49e0e1d5a6081ecc03b9b8d
panweiji <406798106@qq.com>
2025-02-25 11:55:11 +0800

    feat: 强大灵活易用的多端CI/CD共享库流水线

--- src/shared/library/common/Qodana.groovy
+++ src/shared/library/common/Qodana.groovy
@@ -116,8 +116,9 @@ class Qodana implements Serializable {
         // ctx.archiveArtifacts artifacts: "${qodanaReportDir}/**", allowEmptyArchive: true
 
         // 钉钉通知质量报告 形成信息闭环
+        // if ("${ctx.params.IS_DING_NOTICE}" == 'true')  // 是否钉钉通知
         DingTalk.notice(ctx, "${map.ding_talk_credentials_id}", "静态代码分析质量报告 [${ctx.env.JOB_NAME} ${ctx.PROJECT_TAG}](${ctx.env.JOB_URL}/${reportName})  📑",
-                +"\n  ### 代码质量分析结果: [查看](${ctx.env.JOB_URL}/${reportName}) 📈" +
+                "\n  ### 代码质量分析结果: [查看](${ctx.env.JOB_URL}/${reportName}) 📈" +
                         +"\n  ##### 交付可读、易维护和安全的高质量代码 ✨ "
                         + "\n  ###### 执行人: ${ctx.BUILD_USER} \n ###### 完成时间: ${Utils.formatDate()} (${Utils.getWeek(ctx)})", "")
     }
