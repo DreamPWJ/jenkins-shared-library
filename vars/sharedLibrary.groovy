d160bff79e9e12bce5b9a1f9bdb160db9e760be8
panweiji <406798106@qq.com>
2025-04-18 10:25:19 +0800

    feat: 强大灵活易用的多端CI/CD共享库流水线

--- vars/sharedLibrary.groovy
+++ vars/sharedLibrary.groovy
@@ -2027,8 +2027,8 @@ def gitTagLog() {
                 // sh ' git fetch --tags ' // 拉取远程分支上所有的tags 需要设置用户名密码
                 // 获取本地当前分支最新tag名称 git describe --abbrev=0 --tags  获取远程仓库最新tag命令 git ls-remote   获取所有分支的最新tag名称命令 git describe --tags `git rev-list --tags --max-count=1`
                 // 不同分支下的独立打的tag可能导致tag版本错乱的情况  过滤掉非语义化版本的tag版本号
-                // latestTag = Utils.getShEchoResult(this, "git describe --abbrev=0 --tags")
-                latestTag = Git.getGitTagMaxVersion(this)
+                 latestTag = Utils.getShEchoResult(this, "git describe --abbrev=0 --tags")
+                 // Git.getGitTagMaxVersion(this)
 
                 // 生成语义化版本号
                 tagVersion = Utils.genSemverVersion(this, latestTag, gitChangeLog.contains(GlobalVars.gitCommitFeature) ?
