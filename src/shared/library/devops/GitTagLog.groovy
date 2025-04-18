a7adfc749d6032222032fe7e076b70afe3656b38
panweiji <406798106@qq.com>
2025-04-18 11:43:32 +0800

    feat: 强大灵活易用的多端CI/CD共享库流水线

--- src/shared/library/devops/GitTagLog.groovy
+++ src/shared/library/devops/GitTagLog.groovy
@@ -27,6 +27,7 @@ def genTagAndLog(ctx, tagVersion, gitChangeLog, repoUrl, gitCredentialsId) {
                    git config --global user.email "406798106@qq.com"
                    git config --global user.name ${GIT_USERNAME}
                    git config --global pull.rebase false  || true
+                   git config --global push.default simple  || true
                    git fetch --tags --force ${userPassWordUrl} || true
                    git pull ${userPassWordUrl} || true
                    git stash || true
