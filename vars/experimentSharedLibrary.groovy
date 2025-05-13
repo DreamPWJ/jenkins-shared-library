6aed9a6078a271b658b4070a622227181de2341b
panweiji <406798106@qq.com>
2025-05-13 11:33:28 +0800

    feat: 强大灵活易用的多端CI/CD共享库流水线

--- vars/experimentSharedLibrary.groovy
+++ vars/experimentSharedLibrary.groovy
@@ -657,8 +657,8 @@ def test(map) {
     sh "mvn --version"
     sh "java --version"
 
-    // -Dquickly
-    sh "mvnd  clean install -pl pengbo-park/pengbo-park-app -am -Dmaven.compile.fork=true -Dmaven.test.skip=true"
+
+    sh "mvnd clean install -Dquickly -pl pengbo-park/pengbo-park-app -am -Dmaven.compile.fork=true -Dmaven.test.skip=true"
     //sh "mvnd  install"
     //sh "mvn  install"
 
