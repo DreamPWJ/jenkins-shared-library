3fa92d7620c859c099f7321b0c0cece3f228a415
panweiji <406798106@qq.com>
2025-05-19 14:41:51 +0800

    feat: 强大灵活易用的多端CI/CD共享库流水线

--- vars/experimentSharedLibrary.groovy
+++ vars/experimentSharedLibrary.groovy
@@ -686,7 +686,8 @@ def futureLab(map) {
         sh "mvn --version"
         sh "java --version"
 
-        sh "mvnd clean install -pl pengbo-park/pengbo-park-app -am -Dmaven.compile.fork=true -Dmaven.test.skip=true"
+        sh "export MAVEN_OPTS=\"-Djansi.force=true\""
+        sh "mvnd clean install -T2 -pl pengbo-park/pengbo-park-app -am -Dmaven.compile.fork=true -Dmaven.test.skip=true"
         sh "mvn clean install  -pl pengbo-park/pengbo-park-app -am -Dmaven.compile.fork=true -Dmaven.test.skip=true"
         //sh "mvnd  install"
         //sh "mvn  install"
