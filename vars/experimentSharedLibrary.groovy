d8e7ebe2fa1ca45ae9d0d65ea8f7d965c200904a
panweiji <406798106@qq.com>
2025-06-09 09:34:34 +0800

    feat: 强大灵活易用的多端CI/CD共享库流水线

--- vars/experimentSharedLibrary.groovy
+++ vars/experimentSharedLibrary.groovy
@@ -651,6 +651,7 @@ def futureLab(map) {
     addInfoBadge(id: "launch-badge", icon: 'symbol-rocket plugin-ionicons-api', text: '潘维吉同学 正在为您加速部署sit环境 ...')
     sleep 5
     addBadge(id: "version-badge", text: "2.3.6")
+    addInfoBadge(id: "url-badge", text: '访问地址', link: 'https://yuanbao.tencent.com/', target: '_blank')
     removeBadges(id: "launch-badge")
 /*    dir("${env.WORKSPACE}/${GIT_PROJECT_FOLDER_NAME}") {
         // 压缩源码文件 加速传输
