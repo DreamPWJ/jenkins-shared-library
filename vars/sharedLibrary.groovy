98b695a1452f9d93072cf1b4e9b0b122fcb41a53
panweiji <406798106@qq.com>
2025-06-11 17:13:40 +0800

    feat: 强大灵活易用的多端CI/CD共享库流水线

--- vars/sharedLibrary.groovy
+++ vars/sharedLibrary.groovy
@@ -2027,9 +2027,7 @@ def alwaysPost() {
         if ("${IS_PROD}" == 'true') {
             addBadge(id: "version-badge", text: "${tagVersion}", color: 'green', cssClass: 'badge-text--background')
         } else {
-            if ("${PROJECT_TYPE}".toInteger() == GlobalVars.backEnd) {
-                addBadge(id: "env-badge", text: "${releaseEnvironment}".toUpperCase(), cssClass: 'badge-text--background')
-            }
+            addBadge(id: "env-badge", text: "${releaseEnvironment}".toUpperCase(), cssClass: 'badge-text--background')
         }
         addBadge(id: "url-badge", icon: 'symbol-link plugin-ionicons-api', text: '访问地址', link: "${noticeHealthCheckUrl}", target: '_blank')
         removeBadges(id: "launch-badge")
