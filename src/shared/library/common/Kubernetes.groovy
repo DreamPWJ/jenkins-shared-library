f799318bc6a5e628ac485e2b47e317167f367681
panweiji <406798106@qq.com>
2025-06-11 10:58:18 +0800

    feat: 强大灵活易用的多端CI/CD共享库流水线

--- src/shared/library/common/Kubernetes.groovy
+++ src/shared/library/common/Kubernetes.groovy
@@ -224,13 +224,18 @@ class Kubernetes implements Serializable {
 
             def yamlName = "hpa.yaml"
             // 如果cpu或内存达到限额百分之多少 进行自动扩容
-            def cpuHPA = Integer.parseInt("${map.docker_limit_cpu}".replace("m", "")) * 0.8 + "m"
+            def cpuHPAValue = 0.8 // CPU使用多少百分比扩缩容
+            def memoryHPAValue = 0.9 // 内存使用多少百分比扩缩容
+            def defaultHPANum = 1  // 默认扩容个数 加或乘 合理设置 防止资源不足
+
+            def cpuHPA = Integer.parseInt("${map.docker_limit_cpu}".replace("m", "")) * cpuHPAValue + "m"
             // 内存值不支持小数  转成成为M数据
             def memoryUnit = "${map.docker_memory}".contains("G") ? "G" : "M"
-            def memoryHPA = Math.floor(Integer.parseInt("${map.docker_memory}".replace(memoryUnit, "")) * 0.9 * 1024) + "M"
+            def memoryHPA = Math.floor(Integer.parseInt("${map.docker_memory}".replace(memoryUnit, "")) * memoryHPAValue * 1024) + "M"
             def k8sPodReplicas = "${ctx.K8S_POD_REPLICAS}"
             // 最大扩容数量设置为基础pod节点的倍数 默认为2倍或者+1 避免过多扩容节点导致资源耗尽
-            def maxK8sPodReplicas = Integer.parseInt(k8sPodReplicas) + 1 // * 2
+            def maxK8sPodReplicas = Integer.parseInt(k8sPodReplicas) + defaultHPANum // * 2
+
             // 不同配置环境的相同应用 或者 定时任务在应用代码内无分布式处理机制情况
             if ("${ctx.IS_DIFF_CONF_IN_DIFF_MACHINES}" == 'true' && "${ctx.SOURCE_TARGET_CONFIG_DIR}".trim() != "") {
                 if (deployNum != 0) { // 第二次以后环境部署
@@ -240,6 +245,7 @@ class Kubernetes implements Serializable {
                     maxK8sPodReplicas = 1
                 }
             }
+
             def k8sVersion = getK8sVersion(ctx)
             def hpaApiVersion = "v2" // 默认使用新的稳定版本
             if (Utils.compareVersions(k8sVersion, "1.23.0") == -1) { // k8s低版本 使用低版本api
