71e3e2a87701b0130916d37c938f6aa116b87672
panweiji <406798106@qq.com>
2025-06-25 14:29:57 +0800

    feat: 强大灵活易用的多端CI/CD共享库流水线

--- vars/experimentSharedLibrary.groovy
+++ vars/experimentSharedLibrary.groovy
@@ -67,7 +67,7 @@ def call(String type = 'experiment', Map map) {
                         description: '是否在生产环境中自动给Git仓库设置Tag版本和生成CHANGELOG.md变更记录 📄')
                 booleanParam(name: 'IS_DING_NOTICE', defaultValue: "${map.is_ding_notice}", description: "是否开启钉钉群通知 将构建成功失败等状态信息同步到群内所有人 📢 ")
                 choice(name: 'NOTIFIER_PHONES', choices: "${contactPeoples}", description: '选择要通知的人 (钉钉群内@提醒发布结果) 📢 ')
-                file(name: 'MY_FILE', description: '请上传部署包文件')
+                file(name: 'DEPLOY_PACKAGE', description: '请上传部署包文件')
                 //booleanParam(name: 'IS_DEPLOY_MULTI_ENV', defaultValue: false, description: '是否同时部署当前job项目多环境 如dev test等')
             }
 
@@ -665,7 +665,7 @@ def pullProjectCode() {
  */
 def futureLab(map) {
     // 获取上传文件的路径
-    def filePath = params.MY_FILE
+    def filePath = params.DEPLOY_PACKAGE
     // 此时文件已经在工作空间中，路径为filePath
     echo "Uploaded file path: ${filePath}"
 
