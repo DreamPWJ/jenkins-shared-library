437df931674066da4e53f3d6b3d9d23444d1a4ab
panweiji <406798106@qq.com>
2025-07-01 17:43:02 +0800

    feat: 强大灵活易用的多端CI/CD共享库流水线

--- src/shared/library/common/Docker.groovy
+++ src/shared/library/common/Docker.groovy
@@ -119,8 +119,16 @@ class Docker implements Serializable {
             }
 
             if (ctx.IS_PACKAGE_DEPLOY == true) {  // 直接构建包部署方式  如无源码的情况
-                ctx.println("判断文件类型进行特殊化处理: ${ctx.DEPLOY_PACKAGE_FILENAME}")
-            } else if ("${ctx.IS_SOURCE_CODE_DEPLOY}" == 'true') {  // 源码直接部署 无需打包 只需要压缩上传到服务器上执行自定义命令启动
+                def deployPackageFile = "${ctx.DEPLOY_PACKAGE_FILENAME}"
+                ctx.println("判断文件类型进行特殊化处理: ${deployPackageFile}")
+                if (deployPackageFile.endsWith(".jar") || deployPackageFile.endsWith(".war")) {
+                    ctx.mavenPackageLocationDir = ""
+                } else if (deployPackageFile.endsWith(".tar.gz")) {
+
+                }
+            }
+
+            if ("${ctx.IS_SOURCE_CODE_DEPLOY}" == 'true') {  // 源码直接部署 无需打包 只需要压缩上传到服务器上执行自定义命令启动
                 def codeDockerFileName = "Dockerfile.code"
                 def jdkPublisher = "${ctx.JDK_PUBLISHER}"
                 def dockerImagesName = "${jdkPublisher}:${ctx.JDK_VERSION}"
