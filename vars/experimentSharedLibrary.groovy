ca4441603c894802999747f3c9a191b62b71848d
panweiji <406798106@qq.com>
2025-07-14 09:06:49 +0800

    feat: 强大灵活易用的多端CI/CD共享库流水线

--- vars/experimentSharedLibrary.groovy
+++ vars/experimentSharedLibrary.groovy
@@ -182,17 +182,16 @@ def call(String type = 'experiment', Map map) {
                     steps {
                         script {
                             parallel( // 步骤内并发执行
-                                    failFast true,   // 表示其中只要有一个分支构建执行失败，就直接推出不等待其他分支构建
-                                            'CI/CD代码': {
-                                                retry(3) {
-                                                    pullCIRepo()
-                                                }
-                                            },
-                                            '项目代码': {
-                                                retry(3) {
-                                                    pullProjectCode()
-                                                }
-                                            })
+                                    'CI/CD代码': {
+                                        retry(3) {
+                                            pullCIRepo()
+                                        }
+                                    },
+                                    '项目代码': {
+                                        retry(3) {
+                                            pullProjectCode()
+                                        }
+                                    })
                         }
                     }
                 }
