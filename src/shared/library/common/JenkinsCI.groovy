6e4456631993acef057326a13efa9bc12ced8233
panweiji <406798106@qq.com>
2025-07-24 10:25:08 +0800

    feat: 强大灵活易用的多端CI/CD共享库流水线

--- src/shared/library/common/JenkinsCI.groovy
+++ src/shared/library/common/JenkinsCI.groovy
@@ -47,10 +47,9 @@ class JenkinsCI implements Serializable {
             changeLogSets.each { changeLogSet ->
                 changeLogSet.items.each { commit ->
                     // 过滤条件：排除特殊开头的提交
-                    ctx.println("${commit}")
                     ctx.println("${commit.msg}")
-                    ctx.println(GlobalVars.gitCommitChangeLogDocs)
-                    if (!commit.msg.startsWith(GlobalVars.gitCommitChangeLogDocs)) {
+                    ctx.println(ctx.GlobalVars.gitCommitChangeLogDocs)
+                    if (!commit.msg.startsWith(ctx.GlobalVars.gitCommitChangeLogDocs)) {
                         filteredChanges.add([
                                 msg: commit.msg,
                         ])
