41d723ca6436083cd5a0dfb33d96a9bca211364d
panweiji <406798106@qq.com>
2025-07-28 14:31:57 +0800

    feat: 强大灵活易用的多端CI/CD共享库流水线

--- src/shared/library/common/Git.groovy
+++ src/shared/library/common/Git.groovy
@@ -151,9 +151,9 @@ class Git implements Serializable {
             def jenkins = Jenkins.instance.getItem(ctx.env.JOB_NAME)
             def lsb = jenkins.getLastSuccessfulBuild()  // 上次成功的构建
             def lsbTime = lsb.getTime().format("yyyy-MM-dd HH:mm:ss")
-            ctx.println("上次成功构建时间: " + lsbTime)
+            // ctx.println("上次成功构建时间: " + lsbTime)
             gitLogs = Utils.getShEchoResult(ctx, "git log --pretty=format:\"- %s @%an ;\" -n ${maxRecordsNum}  " +
-                    " --since='${lsbTime}' --no-merges | grep \"${GlobalVars.gitCommitChangeLogDocs}\" || true ")
+                    " --since='2025-07-20 12:47:02' --no-merges | grep \"${GlobalVars.gitCommitChangeLogDocs}\" || true ")
 
             // 针对变更记录数组遍历可进行特殊化处理
 /*            def gitLogsArr = gitLogs.split("\\;") as ArrayList
