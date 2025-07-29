b3ca1f7cecefe8b51602cede5bf60f6ae180a891
panweiji <406798106@qq.com>
2025-07-29 11:21:07 +0800

    feat: 强大灵活易用的多端CI/CD共享库流水线

--- _linux/monitor-alarms.sh
+++ _linux/monitor-alarms.sh
@@ -108,11 +108,15 @@ if [ ${DISK_USAGE} -gt ${DISK_USAGE_THRESHOLD} ]; then
              --header 'Content-Type: application/json' \
              --data-raw "$DATA"
         # TODO 记录已发送的记录 防止重复发送  并自动执行脚本清理磁盘空间
+
+        # 自动清理磁盘空间命令
         sudo sh -c "truncate -s 0 /var/lib/docker/containers/*/*-json.log" || true
         find /my -type f -name "*.log" -exec rm -f {} + || true
         find /my -type d -name "log*" -exec rm -rf {} + || true
         docker builder prune --force  || true
         rm -f /var/lib/docker/overlay2/*/diff/etc/nginx/on || true
+        lsof -w | grep 'deleted' | awk '{print $2}' | xargs kill -9  || true
+
         # ./my/clean_disk.sh
         # STATUS_FILE="/tmp/monitor_status"
         # echo "disk_status=0" >> $STATUS_FILE
