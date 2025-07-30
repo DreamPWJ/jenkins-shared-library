ab1377060660e6b008aa09ad413c10091e6f19a5
panweiji <406798106@qq.com>
2025-07-30 16:42:53 +0800

    feat: 强大灵活易用的多端CI/CD共享库流水线

--- vars/experimentSharedLibrary.groovy
+++ vars/experimentSharedLibrary.groovy
@@ -670,17 +670,17 @@ def pullProjectCode() {
         // sh "which git"
         // https仓库下载报错处理 The certificate issuer's certificate has expired.  Check your system date and time.
         sh "git config --global http.sslVerify false || true"
-        // def git = git url: "${REPO_URL}", branch: "${BRANCH_NAME}", credentialsId: "${GIT_CREDENTIALS_ID}"
+        def git = git url: "${REPO_URL}", branch: "${BRANCH_NAME}", credentialsId: "${GIT_CREDENTIALS_ID}"
         // println "${git}"
 
         // 在node节点工具位置选项配置 which git的路径 才能拉取代码!!!
         // 对于大体积仓库或网络不好情况 自定义代码下载超时时间
-        checkout([$class           : 'GitSCM',
-                  branches         : [[name: "*/${BRANCH_NAME}"]],
-                  extensions       : [[$class: 'CloneOption', timeout: 30]],
-                  gitTool          : 'Default',
-                  userRemoteConfigs: [[credentialsId: "${GIT_CREDENTIALS_ID}", url: "${REPO_URL}"]]
-        ])
+//        checkout([$class           : 'GitSCM',
+//                  branches         : [[name: "*/${BRANCH_NAME}"]],
+//                  extensions       : [[$class: 'CloneOption', timeout: 30]],
+//                  gitTool          : 'Default',
+//                  userRemoteConfigs: [[credentialsId: "${GIT_CREDENTIALS_ID}", url: "${REPO_URL}"]]
+//        ])
     } else {  // 基于Git标签代码构建
         println "Git构建标签是: ${params.GIT_TAG} 📇"
         checkout([$class                           : 'GitSCM',
