c7c7aca95cb4bc9aff61a4b6daf5154e5e93a8ea
panweiji <406798106@qq.com>
2025-08-11 09:38:51 +0800

    feat: 强大灵活易用的多端CI/CD共享库流水线

--- src/shared/library/common/JenkinsCI.groovy
+++ src/shared/library/common/JenkinsCI.groovy
@@ -126,18 +126,14 @@ class JenkinsCI implements Serializable {
         def causes = ctx.currentBuild.getBuildCauses()
         // ctx.println ("触发原因：${causes}")
         // 遍历触发原因，判断是否为自动触发类型
-        causes.each { cause ->
-            // 自动触发的常见类型：定时任务、SCM 提交、上游任务触发等
-            if (cause.to instanceof hudson.model.Cause.UserIdCause
-                    || cause instanceof org.jenkinsci.plugins.workflow.cps.replay.ReplayCause
-            ) {
-                // 手动触发
-                ctx.IS_AUTO_TRIGGER = false
-                return cause
-            } else {
-                ctx.IS_AUTO_TRIGGER = true
-                return cause
-            }
+        if (causes.toString().contains("UserIdCause") || causes.toString().contains("ReplayCause")) {
+            // 手动触发
+            ctx.IS_AUTO_TRIGGER = false
+            return causes
+        } else {
+            // 自动触发   自动触发的常见类型：定时任务、SCM 提交、上游任务触发等
+            ctx.IS_AUTO_TRIGGER = true
+            return causes
         }
     }
 
