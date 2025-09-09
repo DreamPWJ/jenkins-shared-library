5397901931ba810ff57f12703857edd8ee681202
panweiji <406798106@qq.com>
2025-09-09 10:08:51 +0800

    feat: 强大灵活易用的多端CI/CD共享库流水线

--- src/shared/library/common/Docker.groovy
+++ src/shared/library/common/Docker.groovy
@@ -338,7 +338,10 @@ export DOCKER_REGISTRY_MIRROR='https://docker.lanneng.tech,https://em1sutsj.mirr
         def cpuCount = Utils.getCPUCount(ctx)
         def memorySize = Utils.getMemorySize(ctx)
         def percentage = 0.8 // 使用多少百分比资源
-        def dockerParams = " -cpus=" + Integer.parseInt(cpuCount) * percentage + " -m " + Math.floor(Integer.parseInt(memorySize) * percentage) + "m "
+        def cpuPercentage = Integer.parseInt(cpuCount) * percentage
+        def memoryPercentage = Math.floor(Integer.parseInt(memorySize) * percentage) + "m"
+
+        def dockerParams = " -cpus=${cpuPercentage}" + " -m ${memoryPercentage} "
         return dockerParams
     }
 
