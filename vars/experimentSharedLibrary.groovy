9f7dfac9ac882116f4c2d678f043e5fdb71c522f
panweiji <406798106@qq.com>
2025-09-25 17:31:25 +0800

    feat: 强大灵活易用的多端CI/CD共享库流水线

--- vars/experimentSharedLibrary.groovy
+++ vars/experimentSharedLibrary.groovy
@@ -806,7 +806,7 @@ def futureLab(map) {
     }*/
 
 
-/*
+
    def mvndVersion = "1.0.2"
     def jdkVersion = "25"
     def dockerImageName = "panweiji/mvnd-jdk"
@@ -824,7 +824,7 @@ def futureLab(map) {
         //sh "mvnd  install"
         //sh "mvn  install"
     }
-*/
+
 
 /*    def gradleVersion = "8" // Gradle版本 要动态配置
     def jdkVersion = "${JDK_VERSION}"
