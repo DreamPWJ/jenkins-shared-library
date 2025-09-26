fa4d70371e8796677c9ab4ca605756590ee9f352
panweiji <406798106@qq.com>
2025-09-26 09:23:05 +0800

    feat: 强大灵活易用的多端CI/CD共享库流水线

--- vars/experimentSharedLibrary.groovy
+++ vars/experimentSharedLibrary.groovy
@@ -807,11 +807,11 @@ def futureLab(map) {
 
 
 
-/*   def mvndVersion = "1.0.2"
-    def jdkVersion = "21"
+   def mvndVersion = "1.0.2"
+    def jdkVersion = "25"
     def dockerImageName = "panweiji/mvnd-jdk"
     def dockerImageTag = "${mvndVersion}-${jdkVersion}"
-    Docker.buildDockerImage(this, map, "${env.WORKSPACE}/ci/Dockerfile.mvnd-jdk", dockerImageName, dockerImageTag, "--build-arg MVND_VERSION=${mvndVersion} --build-arg JDK_VERSION=${jdkVersion}")
+    Docker.buildDockerImage(this, map, "${env.WORKSPACE}/ci/Dockerfile.mvnd-jdk-new", dockerImageName, dockerImageTag, "--build-arg MVND_VERSION=${mvndVersion} --build-arg JDK_VERSION=${jdkVersion}")
 
     docker.image("${dockerImageName}:${dockerImageTag}").inside("-v /var/cache/maven/.m2:/root/.m2") {
 
@@ -823,7 +823,7 @@ def futureLab(map) {
         //sh "mvn clean install  -pl pengbo-park/pengbo-park-app -am -Dmaven.compile.fork=true -Dmaven.test.skip=true"
         //sh "mvnd  install"
         //sh "mvn  install"
-    }*/
+    }
 
 
 /*    def gradleVersion = "8" // Gradle版本 要动态配置
