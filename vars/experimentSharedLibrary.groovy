c14ddebcebd7895ce6e14fe0578aa7e37976e160
panweiji <406798106@qq.com>
2025-09-26 15:00:28 +0800

    feat: 强大灵活易用的多端CI/CD共享库流水线

--- vars/experimentSharedLibrary.groovy
+++ vars/experimentSharedLibrary.groovy
@@ -793,7 +793,7 @@ def futureLab(map) {
         // sh "helm version"
     }*/
 
-/*  def nodeVersion = "${"Node24".replace('Node', '')}"
+    def nodeVersion = "${"Node24".replace('Node', '')}"
     def dockerImageName = "panweiji/node-build"
     def dockerImageTag = "${nodeVersion}"
     Docker.buildDockerImage(this, map, "${env.WORKSPACE}/ci/Dockerfile.node-build", dockerImageName, dockerImageTag, "--build-arg NODE_VERSION=${nodeVersion}")
@@ -803,10 +803,10 @@ def futureLab(map) {
         sh "yarn --version"
         sh "pnpm --version"
      // sh "playwright --version || true"
-    }*/
+    }
 
-   def mvndVersion = "1.0.3"
-    def jdkVersion = "11"
+/*    def mvndVersion = "1.0.3"
+    def jdkVersion = "25"
     def dockerImageName = "panweiji/mvnd-jdk"
     def dockerImageTag = "${mvndVersion}-${jdkVersion}"
     Docker.buildDockerImage(this, map, "${env.WORKSPACE}/ci/Dockerfile.mvnd-jdk-new", dockerImageName, dockerImageTag,
@@ -822,7 +822,7 @@ def futureLab(map) {
         //sh "mvn clean install  -pl pengbo-park/pengbo-park-app -am -Dmaven.compile.fork=true -Dmaven.test.skip=true"
         //sh "mvnd  install"
         //sh "mvn  install"
-    }
+    }*/
 
 
 /*    def gradleVersion = "8" // Gradle版本 要动态配置
