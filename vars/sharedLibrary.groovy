f36f787cbf48b901edf59ab5a061f118fe05bf90
panweiji <406798106@qq.com>
2025-10-09 15:08:27 +0800

    feat: 强大灵活易用的多端CI/CD共享库流水线

--- vars/sharedLibrary.groovy
+++ vars/sharedLibrary.groovy
@@ -1412,7 +1412,7 @@ def mavenBuildProject(map, deployNum = 0, mavenType = "mvn") {
                         sh "${mavenCommandType} clean install -T 2C -Dmaven.compile.fork=true ${isMavenTest} "
                     } else {  // 多模块情况
                         // 单独指定模块构建 -pl指定项目名 -am 同时构建依赖项目模块 跳过测试代码  -T 1C 参数，表示每个CPU核心跑一个工程并行构建
-                        sh "${mavenCommandType} clean install -T 2C -pl ${MAVEN_ONE_LEVEL}${PROJECT_NAME} -am -Dmaven.compile.fork=true -Dmaven.compiler.annotationProcessorPaths=org.projectlombok:lombok:1.18.42 ${isMavenTest} "
+                        sh "${mavenCommandType} clean install -T 2C -pl ${MAVEN_ONE_LEVEL}${PROJECT_NAME} -am -Dmaven.compile.fork=true ${isMavenTest} "
                     }
                 } else {
                     // 基于自定义setting.xml文件方式打包 如私有包等
