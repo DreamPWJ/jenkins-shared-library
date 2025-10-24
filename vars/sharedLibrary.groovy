952648d3e402db0238e1f430e3699192067ecc45
panweiji <406798106@qq.com>
2025-10-24 17:18:42 +0800

    feat: 强大灵活易用的多端CI/CD共享库流水线

--- vars/sharedLibrary.groovy
+++ vars/sharedLibrary.groovy
@@ -328,8 +328,8 @@ def call(String type = 'web-java', Map map) {
                     }
                     /*      agent {
                               dockerfile {
-                                  filename 'Dockerfile.mvnd-jdk' // 在WORKSPACE工作区代码目录
-                                  label "panweiji/mvnd-jdk-${JDK_PUBLISHER}-${JDK_VERSION}:latest"
+                                  filename 'Dockerfile.maven-jdk' // 在WORKSPACE工作区代码目录
+                                  label "panweiji/maven-jdk-${JDK_PUBLISHER}-${JDK_VERSION}:latest"
                                   dir "${env.WORKSPACE}/ci"
                                   additionalBuildArgs "--build-arg MVND_VERSION=1.0.3 --build-arg JDK_PUBLISHER=${JDK_PUBLISHER} --build-arg JDK_VERSION=${JDK_VERSION}"
                                   args " -v /var/cache/maven/.m2:/root/.m2  "
