ddf1145d8bd15504a420dbce145832b5c07dd68e
panweiji <406798106@qq.com>
2025-11-10 10:54:56 +0800

    feat: 强大灵活易用的多端CI/CD共享库流水线

--- vars/experimentSharedLibrary.groovy
+++ vars/experimentSharedLibrary.groovy
@@ -807,8 +807,8 @@ def futureLab(map) {
         // sh "playwright --version || true"
     }*/
 
-/*    def mvndVersion = "1.0.3"
-    def jdkVersion = "25"
+   def mvndVersion = "1.0.3"
+    def jdkVersion = "11"
     def dockerImageName = "panweiji/maven-jdk"
     def dockerImageTag = "${mvndVersion}-${jdkVersion}"
     Docker.buildDockerImage(this, map, "${env.WORKSPACE}/ci/Dockerfile.maven-jdk", dockerImageName, dockerImageTag,
@@ -824,7 +824,7 @@ def futureLab(map) {
         //sh "mvn clean install  -pl pengbo-park/pengbo-park-app -am -Dmaven.compile.fork=true -Dmaven.test.skip=true"
         //sh "mvnd  install"
         //sh "mvn  install"
-    }*/
+    }
 
 
 /*    def gradleVersion = "9" // Gradle版本 要动态配置
