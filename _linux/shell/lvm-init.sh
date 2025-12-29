2fbb7698f5a76f69775528727d0e40adc8c17fca
panweiji <406798106@qq.com>
2025-12-29 15:20:50 +0800

    feat: 强大灵活易用的多端CI/CD共享库流水线

--- _linux/shell/lvm-init.sh
+++ _linux/shell/lvm-init.sh
@@ -48,13 +48,14 @@ systemctl daemon-reload
 umount /dev/vg_data/lv_data
 
 
-# -------------------------- 已设置LVM重新在线扩容硬盘情况 -----------------------------
+# -------------------------- 已设置LVM重新在线扩容硬盘情况  -----------------------------
 # 在线扩容硬盘  重新挂载新磁盘 从这直接开始  注意区分设置的vg和lv名称
 lsblk
-fdisk /dev/sdc # fdisk分区 分别选m n p t(t代表LVM分区表 code设置8e) p w
+fdisk /dev/sdc # fdisk分区 根据fdisk -l显示SIZE的NAME名称 分别选m n p t(t代表LVM分区表 code设置8e) p w
 pvcreate /dev/sdc1 # 创建新物理卷
 vgextend vg_data /dev/sdc1 # 扩展VG到新磁盘 将新盘添加到VG卷管理中  或ubuntu-vg
 
+# 新系统已设置LVM  跳过所有上面步骤 执行下面步骤！！！
 lvextend -L +2046G /dev/vg_data/lv_data # 扩容逻辑卷  -L 参数小于分区存储容量   系统盘默认设置了LVM扩容从这步开始
 xfs_growfs /dev/vg_data/lv_data # xfs扩容文件系统 和 ext格式扩容 resize2fs /dev/ubuntu-vg/ubuntu-lv
 df -h  # 查看扩容后分区大小
