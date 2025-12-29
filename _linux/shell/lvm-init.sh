c10b46d8d0492e696d15ca878be8dd89a3af47e8
panweiji <406798106@qq.com>
2025-12-29 14:26:13 +0800

    feat: 强大灵活易用的多端CI/CD共享库流水线

--- _linux/shell/lvm-init.sh
+++ _linux/shell/lvm-init.sh
@@ -13,7 +13,7 @@ sudo yum install lvm2
 
 # 第一次分LVM区设置 执行下面步骤
 # 分区磁盘 分别选m n p (Select用默认) t(t代表LVM分区表 code设置8e) p w  (fdisk支持2TB大小内分区 新的空GPT分区表解决)
-fdisk /dev/sdb  # sdc 等依次划分磁盘大小
+fdisk /dev/sdb  # sdc 按顺序等依次划分磁盘大小
 
 # 新建一个物理卷 pvdisplay 查看所有物理卷信息
 pvcreate /dev/sdb1
