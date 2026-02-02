75a46e380119aa48aab1d21f32ebe395438248a9
panweiji <406798106@qq.com>
2026-02-02 15:21:56 +0800

    feat: 强大灵活易用的多端CI/CD共享库流水线

--- _docker/others/docker-uninstall.sh
+++ _docker/others/docker-uninstall.sh
@@ -4,11 +4,11 @@
 
 # yum卸载方式
 yum list installed | grep docker
-yum -y remove docker-ce docker-ce-cli.
+yum -y remove docker-ce docker-ce-cli
 rm -rf /var/lib/docker
 
 # apt卸载方式
-sudo apt-get autoremove docker docker-ce docker-engine  docker.io  containerd runc
+sudo apt-get autoremove docker docker-ce docker-engine docker.io containerd runc
 dpkg -l | grep docker && sudo apt-get autoremove docker-ce-*
 sudo rm -rf /etc/systemd/system/docker.service.d
 sudo rm -rf /var/lib/docker
